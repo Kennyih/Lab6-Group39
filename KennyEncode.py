2fc0a1407e13cb5bf7ff749ce4a38e255683b1e3
brian miller <bdouglas.miller@ufl.edu>
2023-03-08 01:37:11 -0500

    Added decode function, made adjustments to encoder to ensure passwords remain at 8 digits.

--- KennyEncode.py
+++ KennyEncode.py
@@ -1,29 +1,70 @@
 #Isabelle Kenny
+#Brian D. Miller II
 
 #Encoder
 def encode(password):
     #Change the string to an array
     arr = [int(x) for x in str(password)]
+           
+    #Checking if password has exactly 8 digits
+    # - Brian
+    if len(password) != 8:
+        print("Error: Password must have exactly 8 digits")
+           
+    else:
 
-   #Has to be 8 characters
-    i = len(arr)
-    for i in range(i,8):
-        arr.append(0)
-    #'Encoding' the password so that each number is +3
-    for j in range(8):
-        temp = arr[j] + 3
-        arr[j] = temp
+       #Has to be 8 characters
+        i = len(arr)
+        for i in range(i,8):
+            arr.append(0)
+        #'Encoding' the password so that each number is +3
+        for j in range(8):
+            #If the encoded number is greater than 9, it will loop back to 0. - Brian
+            #i.e. input = 7, output = 0; input = 8, output = 1; input = 9, output = 2, etc
+            temp = (arr[j] + 3) % 10
+            arr[j] = temp
 
-    #Converting back to string to print
-    new = ''.join(map(str, arr))
+        #Converting back to string to print
+        new = ''.join(map(str, arr))
 
-    print("Your password has been encoded and stored!\n ")
-    return new
+        print("Your password has been encoded and stored!\n ")
+        return new
 
-#Decoder - Partner
+#Decoder - Brian
+def decode(encodedPass):
+
+    #Setting up an exception for blank passwords. - Brian
+    try:
+        if not encodedPass:
+            raise ValueError("Error, No password inputted")
+        
+        #Basically just doing the same thing, but in reverse. - Brian
+        #Change the string to an array
+        arr = [int(x) for x in str(encodedPass)]
+
+       #Has to be 8 characters
+        i = len(arr)
+        for i in range(i,8):
+            arr.append(0)
+        #'Decoding' the password by subtracting 3 from each value.
+        for j in range(8):
+            temp = arr[j] - 3
+            if temp <0:
+                temp = 10 + temp
+            arr[j] = temp
+
+        #Converting back to string to print
+        originalPass = ''.join(map(str, arr))
+        
+        print(f"The encoded pasword is {encodedPass}, and the original password is {originalPass}.")
+
+    except (ValueError):
+        print("Error: No password inputted")
 
 #main
 menu = True
+#Instantiate encodedPass so the program doesn't crash if you try to decode without inputting anything.
+encodedPass = ""
 
 #while true, run menu
 while menu:
